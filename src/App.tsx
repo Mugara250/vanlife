f0fe1737f9f0ab18705319022280f1e80ae991e2
Mbonyumugara Mushi <mbonyumugara52@gmail.com>
2025-08-14 16:37:54 +0200

    fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Vans from "./Vans";
@@ -7,37 +7,35 @@ import "../server";
 function App() {
   return (
     <div className="w-[40%] mx-auto">
-      <BrowserRouter>
-        <header className="bg-[#FFF7ED] flex justify-between py-10 px-6">
-          <Link to="/" className="font-extrabold text-3xl">
-            #VANLIFE
+      <header className="bg-[#FFF7ED] flex justify-between py-10 px-6">
+        <Link to="/" className="font-extrabold text-3xl">
+          #VANLIFE
+        </Link>
+        <nav className="text-base flex gap-x-4">
+          <Link
+            className="text-[#4D4D4D] font-semibold hover:text-black hover:underline"
+            to="/about"
+          >
+            About
           </Link>
-          <nav className="text-base flex gap-x-4">
-            <Link
-              className="text-[#4D4D4D] font-semibold hover:text-black hover:underline"
-              to="/about"
-            >
-              About
-            </Link>
-            <Link
-              to="/vans"
-              className="text-[#4D4D4D] font-semibold hover:text-black hover:underline"
-            >
-              Vans
-            </Link>
-          </nav>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />}></Route>
-          <Route path="/vans:id" element{...< VanDetails />}></Route>
-        </Routes>
-        <footer className="bg-[#252525] text-[#AAAAAA] text-center py-5">
-          &copy; 2022 #VANLIFE
-        </footer>
-      </BrowserRouter>
-    </div>
+          <Link
+            to="/vans"
+            className="text-[#4D4D4D] font-semibold hover:text-black hover:underline"
+          >
+            Vans
+          </Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />}></Route>
+        <Route path="/vans/:id" element={< VanDetails />}></Route>
+      </Routes>
+      <footer className="bg-[#252525] text-[#AAAAAA] text-center py-5">
+        &copy; 2022 #VANLIFE
+      </footer>
+    </div >
   );
 }
 
